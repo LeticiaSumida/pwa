63afba6e1aceb4a8f25836f56a6397f1cea124c3
LeticiaSumida <125376398+LeticiaSumida@users.noreply.github.com>
2023-10-24 08:29:43 -0300

    carousel

--- js/app.js
+++ js/app.js
@@ -1,30 +1,4 @@
-const images = document.querySelectorAll('#slideshow-container img');
-const audio = document.getElementById('audio');
-const playButton = document.getElementById('playButton');
-const pauseButton = document.getElementById('pauseButton');
-let currentIndex = 0;
-
-playButton.addEventListener('click', () => {
-  showImage(currentIndex);
-  startSlideshow();  
-  audio.play();
-});
-
-function showImage(index) {
-    images.forEach((img, i) => {
-        img.style.display = i === index ? 'block' : 'none';
-    });
-}
-
-function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
-}
-
-function startSlideshow() {
-    setInterval(nextImage, 5000); // Change image every 5 seconds (adjust as needed)
-}
-   
+  
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
