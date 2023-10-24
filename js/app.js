dd9e0e2ab47cb2be0c4de22c291d3a1bf930866a
LeticiaSumida <125376398+LeticiaSumida@users.noreply.github.com>
2023-10-24 15:23:05 -0300

    a

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+  class SildeStories{
+    constructor (id) {
+      this.slide = document.querySelector('[data-slide=${id}]');
+      this.active = 0;
+      console.log(this.slide)
+      this.activeSlide(2);
+    }
+
+    activeSlide(index) {
+      this.active = index;
+      this.items = this.slide.querySelector('.slide-items > *');
+      this.items[index].classList.add('active');
+    }
+  }
+
+  new SildeStories ('slides');
   
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
